25ee09e7e5c89536a5f2138df0b3f31504abae45
manoj <manojkolisetty735@gmail.com>
2020-08-28 17:53:08 +0530

    inintial

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyparser.urlencoded({extended: true}));
+
+const https = require("https");
+const { Console } = require('console');
+
+app.get("/",function(req,res){
+
+    res.sendFile( __dirname +"/index.html");
+
+    
+})
+
+app.post("/",function(req, res){
+    //    console.log(req.body.Cityname);
+
+   const query = req.body.Cityname;
+   const apikey = "74dfb052107ca9f01c24e8c1fb4bf6f4";
+   const url = "https://api.openweathermap.org/data/2.5/weather?q="+query + "&appid=" +apikey  ;
+
+   https.get(url, function (resp){
+           console.log(resp.statuscode);
+    
+            resp.on("data",function(data){
+                const weatherdata = JSON.parse(data);
+                
+                const temp = weatherdata.main.temp;
+                const weatherdecription = weatherdata.weather[0].description;
+                // const icon = weatherdata.weather[0].id;
+    
+    
+                res.write(" the weather is   " + weatherdecription + "!");
+                res.write(" the temp in " + query + "is" + temp + "celcius");
+                
+                // res. write(icon);
+                res.send();
+            })
+       })
+    
+
+});
+
+
+//
+app.listen(process.env.PORT || 3000,function(req,res){
+    console.log('server runin on port 3000');
+})
