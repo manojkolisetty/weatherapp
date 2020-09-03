f06b2aa67e3dad75cecf949b9594339c14dd698c
manoj <manojkolisetty735@gmail.com>
2020-09-03 11:20:20 +0530

    units added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyparser.urlencoded({extended: true}));
 
 const https = require("https");
 const { Console } = require('console');
+const { url } = require('inspector');
 
 app.get("/",function(req,res){
 
@@ -20,9 +21,18 @@ app.post("/",function(req, res){
     //    console.log(req.body.Cityname);
 
    const query = req.body.Cityname;
+   const uni = req.body.units;
    const apikey = "74dfb052107ca9f01c24e8c1fb4bf6f4";
-   const url = "https://api.openweathermap.org/data/2.5/weather?q="+query + "&appid=" +apikey  ;
-
+    if (uni == "celcius") {
+        var url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units=metric&appid=74dfb052107ca9f01c24e8c1fb4bf6f4";
+    } else if (uni == "fahrenheit"){
+        var url = "https://api.openweathermap.org/data/2.5/weather?q="+query +"&units=imperial&appid=74dfb052107ca9f01c24e8c1fb4bf6f4"  ;  
+    }
+    else if (uni == "kelvin"){
+        var url = "https://api.openweathermap.org/data/2.5/weather?q="+query + "&appid=" +apikey  ;
+    }
+//    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query + "&appid=" +apikey  ;
+    //   var link = url;
    https.get(url, function (resp){
 
            console.log(resp.statusCode);
@@ -40,11 +50,11 @@ app.post("/",function(req, res){
                 const tempe = weatherdata.main.temp;
                 const weatherdecription = weatherdata.weather[0].description;
                 const icon = weatherdata.weather[0].icon;
-                var resu = (tempe-273.15);
+                // var resu = (tempe-273.15);
     
                 console.log(icon);
                 res.write("<h1> The weather is   " + weatherdecription + "! . </h1>");
-                res.write("<p> The temp in " + query + " is " + resu + " celcius <p>");
+                res.write("<p> The temp in " + query + " is " + tempe +" " + uni+ "<p>");
                 res.write("<img src=  http://openweathermap.org/img/wn/"+icon+"@2x.png>")
                 // res. write(icon);
                 res.send();
@@ -54,7 +64,7 @@ app.post("/",function(req, res){
     
 
 });
-
+// redirecting tp main pagr from fapure on clicking try again
  app.post("/failure",function(read,res){
 
     res.redirect("/")
